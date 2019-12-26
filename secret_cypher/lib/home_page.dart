748afe6daf4b73d4dd966cbbfd37401ebdc0249d
jay <carbonbasednerd@gmail.com>
2019-12-26 17:16:35 -0500

    set up firebase auth and login stuff

---
+++ secret_cypher/lib/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:secret_cypher/authentication.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.auth, this.userId, this.logoutCallback})
+      : super(key: key);
+
+  final BaseAuth auth;
+  final VoidCallback logoutCallback;
+  final String userId;
+
+  @override
+  State<StatefulWidget> createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+      title: new Text('Secret Cypher'),
+      actions: <Widget>[
+        new FlatButton(
+            child: new Text('Logout',
+                style: new TextStyle(fontSize: 17.0, color: Colors.white)),
+            onPressed: signOut)
+      ],
+    ));
+  }
+
+  signOut() async {
+    try {
+      await widget.auth.signOut();
+      widget.logoutCallback();
+    } catch (e) {
+      print(e);
+    }
+  }
+}
