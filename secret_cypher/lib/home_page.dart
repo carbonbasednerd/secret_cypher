b3e852b8ea5778bb6f2d7d7c074493e6061e8511
jay <carbonbasednerd@gmail.com>
2019-12-27 17:24:12 -0500

    added more code codex, ability to enter a message using morse code, real time detection of taps to display character

--- secret_cypher/lib/home_page.dart
+++ secret_cypher/lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:secret_cypher/authentication.dart';
 
@@ -17,6 +19,9 @@ class _HomePageState extends State<HomePage> {
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   String message;
   String morseCharacter;
+  String character = "";
+  int dotDashes = 1;
+  HashMap codex;
 
 
   @override
@@ -24,6 +29,7 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     message = "";
     morseCharacter = "";
+    codex = generateCodex();
   }
 
   @override
@@ -43,15 +49,29 @@ class _HomePageState extends State<HomePage> {
             child: new Center(
                 child: new Column(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: [showMessage(), showLetter(), showControls()]))));
+                    children: [showMessage(), showMorseResults(), showControls()]))));
   }
 
   Widget showMessage() {
     return new Text(message);
   }
 
-  Widget showLetter() {
+  Widget showMorseCharacter() {
     return new Text(morseCharacter);
+  }
+
+  Widget showCharacter() {
+    return new Text(character);
+  }
+
+  Widget showMorseResults() {
+    return new Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        showCharacter(),
+        showMorseCharacter(),
+      ],
+    );
   }
 
   Widget showControls() {
@@ -104,19 +124,46 @@ class _HomePageState extends State<HomePage> {
 
   void enterCharacter() {
     setState(() {
-      message += morseCharacter;
+      message += character;
       morseCharacter = "";
+      dotDashes = 1;
+      character = "";
     });
   }
 
   void updateMessage(bool isDot) {
-    setState(() {
-      if (isDot) {
-        morseCharacter += "*";
-      } else {
-        morseCharacter += "-";
-      }
-    });
+    print("before $dotDashes");
+    if (morseCharacter.length < 5) {
+      setState(() {
+        if (isDot) {
+          morseCharacter += "*";
+          dotDashes = addOne(dotDashes<<=1);
+        } else {
+          morseCharacter += "-";
+          dotDashes <<= 1;
+        }
+        print(codex.length);
+        character = codex[dotDashes] ?? "";
+        print("After $dotDashes");
+      });
+    }
+  }
+
+  static int addOne(int x)
+  {
+    int m = 1;
+
+    // Flip all the set bits
+    // until we find a 0
+    while( (x & m) >= 1)
+    {
+      x = x ^ m;
+      m <<= 1;
+    }
+
+    // flip the rightmost 0 bit
+    x = x ^ m;
+    return x;
   }
 
   void addSpace() {
@@ -132,5 +179,46 @@ class _HomePageState extends State<HomePage> {
     } catch (e) {
       print(e);
     }
+  }
+
+  HashMap generateCodex() {
+    HashMap map = new HashMap();
+    map[6] = "A";
+    map[23] = "B";
+    map[21] = "C";
+    map[11] = "D";
+    map[3] = "E";
+    map[29] = "F";
+    map[9] = "G";
+    map[31] = "H";
+    map[7] = "I";
+    map[24] = "J";
+    map[10] = "K";
+    map[27] = "L";
+    map[4] = "M";
+    map[5] = "N";
+    map[8] = "O";
+    map[25] = "P";
+    map[18] = "Q";
+    map[13] = "R";
+    map[15] = "S";
+    map[10] = "T";
+    map[14] = "U";
+    map[30] = "V";
+    map[12] = "W";
+    map[22] = "X";
+    map[20] = "Y";
+    map[19] = "Z";
+    map[48] = "1";
+    map[56] = "2";
+    map[60] = "3";
+    map[62] = "4";
+    map[63] = "5";
+    map[47] = "6";
+    map[39] = "7";
+    map[35] = "8";
+    map[33] = "9";
+    map[32] = "0";
+    return map;
   }
 }
